7281aed82ec799eff30edcaefe7456a83003410a
Oybek <programmingiswonderful@gmail.com>
2022-09-07 11:10:10 +0500

    config is ready

---
+++ internal/config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"log"
+	"sync"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	IsDebug       bool `yaml:"is_debug" env:"ST_BOT_IS_DEBUG" env-default:"false"`
+	IsDevelopment bool `yaml:"is_development" env:"ST_BOT_IS_DEVELOPMENT" env-default:"false"`
+	Telegram      struct {
+		Token string `yaml:"token" env:"ST_BOT_TELEGRAM_TOKEN" env-required:"true"`
+	}
+	RabbitMQ struct {
+		Host     string `yaml:"host" env:"ST_BOT_RABBIT_HOST" env-required:"true"`
+		Port     string `yaml:"port" env:"ST_BOT_RABBIT_PORT" env-required:"true"`
+		Username string `yaml:"username" env:"ST_BOT_RABBIT_USERNAME" env-required:"true"`
+		Password string `yaml:"password" env:"ST_BOT_RABBIT_PASSWORD" env-required:"true"`
+		Consumer struct {
+			Youtube            string `yaml:"youtube" env:"ST_BOT_RABBIT_CONSUMER_YOUTUBE" env-required:"true"`
+			Imgur              string `yaml:"imgur" env:"ST_BOT_RABBIT_CONSUMER_IMGUR" env-required:"true"`
+			MessagesBufferSize int    `yaml:"messages_buff_size" env:"ST_BOT_RABBIT_CONSUMER_MBS" env-default:"100"`
+		} `yaml:"consumer"`
+		Producer struct {
+			Youtube string `yaml:"youtube" env:"ST_BOT_RABBIT_PRODUCER_YOUTUBE" env-required:"true"`
+			Imgur   string `yaml:"imgur" env:"ST_BOT_RABBIT_PRODUCER_IMGUR" env-required:"true"`
+		} `yaml:"producer"`
+	}
+	AppConfig AppConfig `yaml:"app"`
+}
+
+type AppConfig struct {
+	EventWorkers struct {
+		Youtube int `yaml:"youtube" env:"ST_BOT_EVENT_WORKERS_YT" env-default:"3"`
+		Imgur   int `yaml:"imgur" env:"ST_BOT_EVENT_WORKERS_IMGUR" env-default:"3"`
+	} `yaml:"event_workers"`
+	LogLevel string `yaml:"log_level" env:"ST_BOT_LOG_LEVEL" env-default:"error"`
+}
+
+var instance *Config
+var once sync.Once
+
+func GetConfig() *Config {
+	once.Do(func() {
+		instance = &Config{}
+
+		if err := cleanenv.ReadEnv(instance); err != nil {
+			help, _ := cleanenv.GetDescription(instance, nil)
+			log.Print(help)
+			log.Fatal(err)
+		}
+	})
+	return instance
+}
